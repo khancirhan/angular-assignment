e1331586749b39641cb9989c478c856f281c45cd
khan cirhan <khancirhan@gmail.com>
2020-07-25 11:49:56 +0530

    Generate All Components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,26 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { SidebarComponent } from './components/sidebar/sidebar.component';
+import { NavbarComponent } from './components/navbar/navbar.component';
+import { AboutComponent } from './components/about/about.component';
+import { TrendingComponent } from './components/trending/trending.component';
+import { PostsComponent } from './components/posts/posts.component';
+import { ActionsComponent } from './components/actions/actions.component';
+import { FollowersComponent } from './components/followers/followers.component';
+import { PremiumComponent } from './components/premium/premium.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    SidebarComponent,
+    NavbarComponent,
+    AboutComponent,
+    TrendingComponent,
+    PostsComponent,
+    ActionsComponent,
+    FollowersComponent,
+    PremiumComponent
   ],
   imports: [
     BrowserModule
